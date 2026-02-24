3d0753456bb011595501c6656d679ba54d5af508
GiordanoPagano <159129735+GiordanoPagano@users.noreply.github.com>
2026-02-24 17:06:39 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# Medical-Robotics-Project
+# Hand Motion Reconstruction from Minimal Input through Latent-Space Learning
+
+We address the problem of real-time motion retargeting for a virtual human hand using the WeART TouchDIVER G1 haptic glove and a data-driven approach based on a neural network. In order to exploit the Hand Synergies and improve both generalization and efficiency, we apply dimensionality reduction techniques such as Principal Component Analysis (PCA) and Autoencoders. 
+
+# Steps for Motion Retargeting with Built-In functions of Weart SDK: 
+
+1. Download Unity Hub 3.0: https://unity.com/download
+2. Download the Weart Unity SDK: https://weart.it/repository/downloads/unity/WEART_SDK_Unity_v2.0.0.zip and extract folder
+3. Create New Project
+4. Go to: Window->Package Manager-> + -> Add package from disk -> select extracted folder from downloaded zip
+5. Go to: WEART -> Add Weart Startup Components
+6. From Hierarchy and go to: WEART-> Hands -> WEARTLeftHand -> go to Inspector and set the Tracking Source as WeArtController
+7. Start PLAY MODE 
+
+# Steps for DATASET creation
+
+1. Start PLAY MODE
+2. From Hierarchy and go to: WEART-> Hands -> WEARTLeftHand -> go to Inspector -> Add Component -> Script -> Hand Data Logger
+3. From Hierarchy -> Hands-> WEARTLeftHand -> HandRig->HandRoot->DEF-hand.R/DEF-thumb.01.R/DEF-thumb.02.R/DEF-thumb.03.R select LeftHapticThumb and slide it to "Thumb Closure" in Lefthand Inspector in Hand Data Logger section (look for LeftHapticIndex and LeftHapticMiddle, respectively) 
+4. From Hierarchy -> Hands-> WEARTLeftHand -> HandRig->HandRoot->DEF-hand.R/ select DEF-thumb.01.R and slide it to "Thumb 1" in the LeftHand Inspector in the Hand Data Logger section (look for all the remaining joints and do the same)
+5. You should see from the Unity console that the CSV file has been created, you can start moving your hand, data is being registered 
+6. Once you stop the PLAY MODE, the CSV file is saved
+7. You can find the CSV file in AppData(W + R) -> LocalLow-> DefaultCompany-> Unity Project folder
+
+# Steps for REAL-TIME PREDICTION using NN (FCNN or Transformer)
+   a. Direct PCA Output:  We reduced the dataset using PCA and trained the NN to predict the PCA components. The joint predictions are obtained by using the PCA inverse transform.
+   
+      1. In the folder "training_results/training_synergies_results" you can find the weights and parameters for the trained models (FCNN or Transformer) using different number of PCA components: 10,                 15, 30, 45.
+      
+      2. To replicate the results, you can run "python main_synergies.py --info_path training_results\training_synergies_results\training_20250521_150116\training_info.txt" (example).
+      
+      3. Once you obtain the message: "Server ready (Model: {model_type} | Fixed indices: {fix_indices})...", you can start PLAY MODE on Unity and run the hand simulation.
+
+   b. PCA-Based Loss Only:  We used the whole dataset to train the NN, and we used the dimensionality reduction only in the loss function (PCA). The NN outputs the whole set of joints predictions.
+   
+      1. In the folder "training_results/training_losspca_results" you can find the weights and parameters for the trained models (FCNN or Transformer) using different number of PCA components: 10,                   15, 30, 45.
+      
+      2. To replicate the results, you can run "python main.py --info_path training_results/training_losspca_results/training_20250606_191433/training_info.txt" (example). 
+      
+      3. Once you obtain the message: "Server ready (Model: {model_type} | Fixed indices: {fix_indices})...", you can start PLAY MODE on Unity and run the hand simulation.
+
+   c. Autoencoder-Based Loss:  We used the whole dataset to train the NN, and we used the dimensionality reduction only in the loss function (Autoencoder). Also added a constraint term in the loss to              prevent unrealistic joint configurations. The NN outputs the whole set of joints predictions.
+   
+      1. In the folder "training_results/training_latentspace_results" you can find the weights and parameters for the trained models (FCNN or Transformer) using different dimensions for the latent space:            10, 15, 30, 45.
+      
+      2. To replicate the results, you can run "python main.py --info_path training_results/training_latentspace_results/FCNN/training_20250606_181904/training_info.txt" (example).
+      
+      3. Once you obtain the message: "Server ready (Model: {model_type} | Fixed indices: {fix_indices})...", you can start PLAY MODE on Unity and run the hand simulation.
+   
+ # Training files
+
+   1. Direct PCA Output --> train_synergies.py
+   2. PCA-Based Loss Only --> train_losspca.py
+   3. Autoencoder-Based Loss --> train_latentspace_input.py
+   4. Autoencoder (alone) --> train_AE_VAE.py
+
+ 
+
+
+
+   
+
---
+++ train_latentspace_input.py
@@ -0,0 +1,311 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import joblib
+from HandPoseClass import *
+from scipy.stats import zscore
+import argparse
+import os
+from datetime import datetime
+
+def create_training_directory(base_dir="training_results/training_latentspace_results"):
+    # Create a unique directory name with timestamp
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    training_dir = os.path.join(base_dir, f"training_{timestamp}")
+    os.makedirs(training_dir, exist_ok=True)
+    return training_dir
+
+def save_training_info(training_dir, info_dict):
+    # Save training info to a text file
+    info_file = os.path.join(training_dir, "training_info.txt")
+    with open(info_file, 'w') as f:
+        for key, value in info_dict.items():
+            f.write(f"{key}: {value}\n")
+
+def generate_sincos_dataset(Y, fix_indices):
+    mixed_columns = []
+    for i in range(Y.shape[1]):
+        if i in fix_indices:
+            sin_val = np.sin(np.deg2rad(Y[:, [i]]))
+            cos_val = np.cos(np.deg2rad(Y[:, [i]]))
+            mixed_columns.extend([sin_val, cos_val])
+        else:
+            mixed_columns.append(Y[:, [i]])
+    return np.concatenate(mixed_columns, axis=1)
+
+
+def load_data(csv_path, closure_columns, fix_indices, z_thresh=2.5):
+    print("Loading Dataset from csv file...")
+    data = pd.read_csv(csv_path)
+    data.columns = data.columns.str.strip()
+    data = data.iloc[:, 1:]  # remove the first column (time)
+    joint_columns = [c for c in data.columns if c not in closure_columns]
+    X = data[closure_columns].values
+    Y = data[joint_columns].values
+
+    X, Y = remove_outliers_zscore(X, Y, z_thresh)
+    Y_sincos = generate_sincos_dataset(Y, fix_indices)
+
+    scaler = StandardScaler().fit(Y_sincos)
+    Y_scaled = scaler.transform(Y_sincos)
+
+    print("All components:", Y_scaled.shape[1])
+
+    return X, Y_scaled, scaler
+
+def find_min_max(Y):
+
+    min_vals = torch.tensor(np.min(Y, axis=0), dtype=torch.float32)
+    max_vals = torch.tensor(np.max(Y, axis=0), dtype=torch.float32)
+    return min_vals, max_vals
+
+def encode_with_autoencoder(Y_tensor, model_ae):
+    return model_ae.encode(Y_tensor)
+
+
+
+def remove_outliers_zscore(X, Y, threshold):
+    z_scores = np.abs(zscore(Y, axis=0))
+    mask = (z_scores < threshold).all(axis=1)
+    return X[mask], Y[mask]
+
+
+def prepare_dataloaders(X, Y, batch_size=64):
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=42)
+    train_loader = DataLoader(TensorDataset(torch.FloatTensor(Xtrain), torch.FloatTensor(Ytrain)), batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(TensorDataset(torch.FloatTensor(Xtest), torch.FloatTensor(Ytest)), batch_size=batch_size)
+    return train_loader, test_loader
+
+
+def train(model, train_loader, test_loader, optimizer, scheduler, loss_fn, min_vals, max_vals,epochs=300, save_path="FCNN_VAE.pth", use_constraint_loss=False): 
+    train_losses, test_losses = [], []
+    best_loss, best_model_state = float('inf'), None
+
+    for epoch in range(epochs):
+        model.train()
+        train_loss = sum_step_loss(model, train_loader, loss_fn, min_vals, max_vals, optimizer, training=True, use_constraint_loss=False)
+
+        model.eval()
+        test_loss = sum_step_loss(model, test_loader, loss_fn, min_vals, max_vals, optimizer, training=False, use_constraint_loss=False)
+
+        scheduler.step(test_loss)
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+
+        if test_loss < best_loss:
+            best_loss = test_loss
+            best_model_state = model.state_dict()
+            #torch.save(best_model_state, save_path)
+
+        if epoch % 20 == 0:
+            lr = optimizer.param_groups[0]['lr']
+            print(f"Epoch {epoch:3d} | LR: {lr:.5f} | Train Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f}")
+
+    if best_model_state:
+        model.load_state_dict(best_model_state)
+
+    return train_losses, test_losses
+
+
+def sum_step_loss(model, loader, loss_fn, min_vals, max_vals, optimizer=None, training=False, use_constraint_loss=False):
+    total_loss = 0.0
+    for xb, yb in loader:
+        preds = model(xb)
+
+        # Move constraints to correct device
+        device = preds.device
+        min_dev = min_vals.to(device)
+        max_dev = max_vals.to(device)
+
+        # 1. Original AE-encoded loss
+        loss_main = loss_fn(preds, yb)
+
+        # 2. Constraint loss
+        loss_constraint = joint_constraint_loss(preds, min_dev, max_dev)
+
+        # 3. Combine
+        #loss = loss_main + loss_constraint
+        loss = loss_main
+        if use_constraint_loss:
+            loss += loss_constraint
+            
+        if training:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        total_loss += loss.item() * xb.size(0)
+    return total_loss / len(loader.dataset)
+
+
+def mse_loss_with_encoder(preds, targets, encoder_model):
+    preds_encoded = encode_with_autoencoder(preds, encoder_model)
+    targets_encoded = encode_with_autoencoder(targets, encoder_model)
+    return torch.nn.functional.mse_loss(preds_encoded, targets_encoded)
+
+
+def joint_constraint_loss(preds, min_tensor, max_tensor):
+    lower_violation = torch.relu(min_tensor - preds)
+    upper_violation = torch.relu(preds - max_tensor)
+    return torch.mean(lower_violation**2 + upper_violation**2)
+
+
+def weighted_mse_loss(preds, targets, fix_indices):
+    weights = torch.ones_like(preds)
+    mixed_col = 0
+    for i in range(45):
+        if i in fix_indices:
+            weights[:, mixed_col: mixed_col + 1] = 2.0
+            mixed_col += 2
+        else:
+            mixed_col += 1
+    return torch.mean(weights * (preds - targets) ** 2)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train hand pose model with AE encoder-based loss.")
+    parser.add_argument('--model_reduction', type=str, help='Choose the model: AE/VAE')
+    parser.add_argument('--latent_dim', type=int, default=30, help='Latent space dimension for AE or VAE')
+    parser.add_argument('--ae_model_path', type=str, required=True, help='Path to pre-trained AE or VAE model')
+    parser.add_argument('--model', type=str, help='Choose the model: FCNN/Transformer')
+    parser.add_argument('--z_thresh', type=float, default=2.5, help='Z-score threshold for outlier removal')
+    parser.add_argument('--epochs', type=int, default=300, help='Number of training epochs')
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
+    #parser.add_argument('--save_model', type=str, default=None, help='Optional custom model save path')
+    parser.add_argument('--plot_mse', action='store_true', help='Plot MSE loss curves')
+    parser.add_argument('--use_constraint_loss', action='store_true', help='Use constraint loss in training')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    print("\nHelping prompt : ")
+    print("python train.py --model FCNN --model_reduction AE --use_constraint_loss \n")
+
+    print("----------- Training Configuration -----------")
+    print(f"Z-score outlier threshold: {args.z_thresh}")
+    print(f"Epochs:                    {args.epochs}")
+    print(f"Batch size:                {args.batch_size}")
+    print(f"Plot loss curve:           {'Yes' if args.plot_mse else 'No'}")
+    print(f"Use Constraint loss:       {'Yes' if args.use_constraint_loss else 'No'}")
+    print("---------------------------------------------")
+
+    # Create training directory
+    training_dir = create_training_directory()
+    print(f"All training results will be saved in: {training_dir}")
+
+    closure_columns = ['ThumbClosure', 'IndexClosure', 'MiddleClosure', 'ThumbAbduction']
+    z_thresh = args.z_thresh
+    fix_Indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 13, 14, 16, 17, 25, 26, 34, 43]
+    latent_dimension = args.latent_dim
+    X, Y, scaler_y = load_data('dataset/hand_dataset_all_fingers.csv', closure_columns, fix_Indices, z_thresh)
+    
+    min_vals, max_vals = find_min_max(Y)
+    training_info = {
+    "Model": args.model,
+    "Z-score Threshold": args.z_thresh,
+    "Epochs": args.epochs,
+    "Batch Size": args.batch_size,
+    "Fixed Indices": fix_Indices,
+    "Number of Fixed Indices": len(fix_Indices),
+    "Input Dimension": 4,
+    "Output Dimension": Y.shape[1],
+    "Dataset": 'dataset/hand_dataset_all_fingers.csv',
+    "Closure Columns": closure_columns,
+    "Training Start Time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+    
+    #joblib.dump(scaler_y, "scaler_AE.save")
+    if args.model_reduction == 'AE':
+        model_reduction = HandPoseAE(input_dim=Y.shape[1], latent_dim = latent_dimension)
+        #model_save_path = os.path.join(training_dir, args.save_model if args.save_model else f"{args.model}_{latent_dimension}_latentdim.pth")
+        #model_reduction.load_state_dict(torch.load("HandPoseAE_2.pth"))   
+    elif args.model_reduction == 'VAE':
+        model_reduction = HandPoseVAE(input_dim=Y.shape[1], latent_dim=latent_dimension)
+        #model_save_path = os.path.join(training_dir, args.save_model if args.save_model else f"{args.model}_{latent_dimension}_latentdim.pth")
+        #model_reduction.load_state_dict(torch.load("HandPoseVAE_2.pth"))
+   
+    model_reduction.load_state_dict(torch.load(args.ae_model_path))
+    training_info["AE/VAE Model Used"] = args.ae_model_path
+    model_reduction.eval()
+    loss_type = lambda preds, targets: mse_loss_with_encoder(preds, targets, model_reduction)
+    #save_path = args.save_model if args.save_model else args.model + f"_{args.model_reduction}.pth"
+    #save_path = model_save_path;
+
+    training_info["Latent Space Dimension"] = latent_dimension
+    training_info["Model Reduction Path"] = args.ae_model_path  # AE or VAE
+
+    output_dim = Y.shape[1]
+    train_loader, test_loader = prepare_dataloaders(X, Y, batch_size=args.batch_size)
+    training_info["Final Output Dimension"] = output_dim
+    regression_model_path = os.path.join(training_dir, f"{args.model}_{args.model_reduction}_regression.pth")
+
+    if args.model == 'FCNN':
+        model = HandPoseFCNN(input_dim=4, output_dim=output_dim)
+    elif args.model == 'Transformer':
+        model = HandPoseTransformer(input_dim=4, fix_indices=fix_Indices)
+    
+    print("This is the dataset:", training_info["Dataset"])
+
+    print(f"\nThis is the model: \n\n{model}")
+    training_info["Regression Model Architecture"] = str(model)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10, factor=0.5, verbose=True)
+    
+    start_time = datetime.now()
+    train_losses, test_losses = train(
+        model, train_loader, test_loader,
+        optimizer, scheduler,
+        loss_fn=loss_type,
+        min_vals=min_vals,
+        max_vals=max_vals,
+        epochs=args.epochs,
+        use_constraint_loss=args.use_constraint_loss
+    )
+    duration = datetime.now() - start_time
+    # Update training info with final results
+    training_info["Min Values"] = min_vals.tolist()
+    training_info["Max Values"] = max_vals.tolist()
+    training_info["Training Start Time"] = start_time.strftime('%Y-%m-%d %H:%M:%S')
+    training_info["Training Duration"] = str(duration)
+    training_info["Best Test Loss"] = min(test_losses)
+    training_info["Final Learning Rate"] = optimizer.param_groups[0]['lr']
+    torch.save(model.state_dict(), regression_model_path)
+    training_info["Regression Model Save Path"] = regression_model_path
+
+    training_info["Constraint Loss Used"] = args.use_constraint_loss
+    # Save training info
+    save_training_info(training_dir, training_info)
+
+    if args.plot_mse:
+
+        plot_dir = os.path.join(training_dir, "plots")
+        os.makedirs(plot_dir, exist_ok=True)
+        plt.figure(figsize=(10, 5))
+        plt.plot(train_losses, label='Train Loss')
+        plt.plot(test_losses, label='Test Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.title('MSE Loss Curve (AE encoder-based)')
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plot_path = os.path.join(plot_dir, "mse_loss_curve_latentspace.png")
+        #plt.savefig("plots/mse_loss_curve.png")
+        plt.savefig(plot_path)
+        plt.close()
+        training_info["Plot Path"] = plot_path
+
+    print("Training complete.")
+    print(f"Best test loss: {min(test_losses):.6f}")
+    print("\nTraining Summary:")
+    for key, value in training_info.items():
+        print(f"{key}: {value}")
